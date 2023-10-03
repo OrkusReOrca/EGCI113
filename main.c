fbc570dc38f3d64274576468ba70f4be4ab31f93
OrkusReOrca <146055263+OrkusReOrca@users.noreply.github.com>
2023-10-03 10:54:46 +0700

    Polynomial finder, all ans mode
    add alternative answers
    - a=0, a,b=0, root negative, one answer, two answer

--- main.c
+++ main.c
@@ -1,21 +1,37 @@
-#include <stdio.h>
 #include <stdlib.h>
-#define PI 3.14
+#include <math.h>
 
-int main()
+void main()
 {
-    int a,b,c,min,max;
-    printf("Input 3 numbers: ");
-    scanf("%d %d %d",&a,&b,&c);
-    if(a<=b && a<=c) min=a;
-    else if(b<=c) min=b;
-    else min=c;
+    double a,b,c,ans1,ans2,chckSq;
+    printf("Input the a,b,c of polynomials below in this form (a b c):\n");
+    scanf("%lf %lf %lf",&a,&b,&c);
+    puts("");
 
-    if(a>=b && a>=c) max=a;
-    else if(b>=c) max=b;
-    else max=c;
+    //if a and b are zero
+    if(a==0 && b==0){
+        printf("Answer to the polynomial provided is %.2f", c);
 
-    printf("Min is %d\n",min);
-    printf("Mas is %d\n",max);
-    return main;
+    //if a are zero
+    }else if (a==0){
+       ans1= (-c)/b;
+       printf("Answer to the polynomial provided is %.2f", ans1);
+
+    //all are non zero
+    }else{
+        chckSq= (pow(b,2))-(4*a*c);
+
+        //undefined due to negative root
+        if(chckSq<0){
+            printf("Answer to the polynomial provided is UNDEFINE due to negative square root");
+        //NORMAL define
+        }else{
+            ans1= (-b+(sqrt((pow(b,2))-(4*a*c))))/(2*a);
+            ans2= (-b-(sqrt((pow(b,2))-(4*a*c))))/(2*a);
+            if(ans1==ans2) printf("Answer to the polynomial provided is %.2f", ans1);
+            else printf("Answer to the polynomial provided is %.2f and %.2f", ans1, ans2);
+        }
+    }
+    puts("");
+    return 0;
 }
