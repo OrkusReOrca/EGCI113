00ceab125588c0da421302120832251712d753c9
OrkusReOrca <146055263+OrkusReOrca@users.noreply.github.com>
2023-10-03 11:51:26 +0700

    polynomail equation output

--- main.c
+++ main.c
@@ -3,35 +3,32 @@
 
 void main()
 {
-    double a,b,c,ans1,ans2,chckSq;
+    double ans1,ans2;
+    int a,b,c;
     printf("Input the a,b,c of polynomials below in this form (a b c):\n");
-    scanf("%lf %lf %lf",&a,&b,&c);
-    puts("");
-
-    //if a and b are zero
-    if(a==0 && b==0){
-        printf("Answer to the polynomial provided is %.2f", c);
-
-    //if a are zero
-    }else if (a==0){
-       ans1= (-c)/b;
-       printf("Answer to the polynomial provided is %.2f", ans1);
-
-    //all are non zero
-    }else{
-        chckSq= (pow(b,2))-(4*a*c);
-
-        //undefined due to negative root
-        if(chckSq<0){
-            printf("Answer to the polynomial provided is UNDEFINE due to negative square root");
-        //NORMAL define
-        }else{
-            ans1= (-b+(sqrt((pow(b,2))-(4*a*c))))/(2*a);
-            ans2= (-b-(sqrt((pow(b,2))-(4*a*c))))/(2*a);
-            if(ans1==ans2) printf("Answer to the polynomial provided is %.2f", ans1);
-            else printf("Answer to the polynomial provided is %.2f and %.2f", ans1, ans2);
-        }
+    scanf("%d %d %d",&a,&b,&c);
+    double aP=a,bP=b,cP=c;
+    ans1= (-bP+(sqrt((pow(bP,2))-(4*aP*cP))))/(2*aP);
+    ans2= (-bP-(sqrt((pow(bP,2))-(4*aP*cP))))/(2*aP);
+    switch(a){
+        case 0: break;
+        case 1: printf("x^2"); break;
+        case -1: printf("-x^2"); break;
+        default: printf("%dx^2",a);
     }
+    if(b>0) printf("+");
+    switch(b){
+        case 0: break;
+        case 1: printf("x"); break;
+        case -1: printf("-x"); break;
+        default: printf("%dx",b);
+    }
+    if(c>0) printf("+");
+    switch(c){
+        case 0: break;
+        default: printf("%d",c);
+    }
+    printf(" = %lf and %lf",ans1,ans2);
     puts("");
     return 0;
 }
